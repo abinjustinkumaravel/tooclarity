87fcbf873baed53b859ce8fcc991c4aebed37105
Abin Justin Kumaravel <abinjustinkumaravel@gmail.com>
2024-09-11 03:27:14 +0000

    Changes made in Landing

--- src/App.js
+++ src/App.js
@@ -15,9 +15,8 @@ function App() {
     {/* <img src={BG} alt=".." className=" w-full h-full absolute -z-40"></img> */}
     <Lefttext/>
       <div className="text-center items-center sm:w-[50%]">
-        <h2 className="text-[20px] lg:text-[25px] m-[20px] text-white tracking-[10px]">World<span className=" text-[25px] text-[rgba(0,0,128)] tracking-[10px]"> Education </span> On One Scale</h2>
         <h1 className="sm:tracking-[50px] justify-center items-center text-center tracking-[20px] font-extrabold md:text-4xl sm:text-[30px] text-[25px] text-white">COMMING SOON</h1>
-        <h3 className="sm:tracking-[10px] tracking-[5px] m-[50px] sm:text-1xl text-15px text-white">You're Gonna Like Our Website</h3>
+        <h3 className="sm:tracking-[10px] tracking-[5px] m-[50px] sm:text-1xl text-15px text-white">World Education On One Scale</h3>
       </div>
      <Social/> 
     </div>
