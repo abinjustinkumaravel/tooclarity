534c072fbc30501c35cd3ca2e29f27eaa3e0036a
Abin Justin Kumaravel <abinjustinkumaravel@gmail.com>
2024-09-13 04:24:21 +0000

    Landing page fixed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,9 +7,9 @@ const navbar = () => {
     <div className='flex bg-transparent w-full items-center justify-between absolute sm:p-[50px] sm:pr-[70px] sm:pl-[70px] md:pl-[200px] md:pr-[200px] p-[20px] '>
       {/* <img className="bg-transparent sm:w-52 w-36" src={Logo} alt=""/> */}
 
-      <h1 className='text-white font-bold text-[50px]'>Tooclarity </h1>
+      <h1 className='text-white font-bold text-[35px]'>TooClarity </h1>
 
-      <div className='text-white flex gap-[20px] text-[20px] font-bold'>
+      <div className='text-white flex gap-[20px] text-[15px] font-semibold '>
         <a href='/'>
           Home
         </a>
