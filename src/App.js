e52a7bdbe865005ddf46b32ff852172bf52c07ae
Abin Justin Kumaravel <abinjustinkumaravel@gmail.com>
2024-09-15 04:36:06 +0000

    Resposive done with lading page

--- src/App.js
+++ src/App.js
@@ -9,18 +9,18 @@ function App() {
     <Navbar/>
     <Circle/>
     <div className="flex items-center justify-center h-full bg-black bg-gradient-to-r from-black via-[rgba(0,0,128,0.1)] to-black">
-      <div className="text-center items-center sm:w-[50%] z-40">
-        <h1 className="sm:tracking-[50px] justify-center items-center text-center tracking-[20px] font-extrabold md:text-4xl sm:text-[30px] text-[25px] text-white">COMMING SOON</h1>
-        <h3 className="sm:tracking-[10px] tracking-[5px] m-[50px] sm:text-1xl text-15px text-white">World Education On One Scale</h3>
+      <div className="text-center items-center  z-40">
+        <h1 className="sm:tracking-[50px] justify-center m-[50px] items-center text-center tracking-[20px] font-extrabold md:text-4xl sm:text-[30px] text-[25px] text-white">COMMING SOON</h1>
+        <h3 className="sm:tracking-[10px] tracking-[5px] m-[50px] sm:text-1xl text-15px text-white">World Education on One Scale</h3>
         <label className="text-white" >Lunching...</label>
         <div className="flex justify-center items-center gap-[10px]"> 
-        <progress className="w-[200px] h-[10px] rounded-full border-[rgba(0,0,128)] border-[1px] bg-black bg-gradient-to-r from-black via-[rgba(0,0,128,0.1)] to-black" id="file" value="10" max="100"></progress> <p className='text-white text-[15px]'>10 %</p>
+        <progress className="w-[200px] h-[10px] rounded-full border-[rgba(0,0,128)] border-[1px] bg-black bg-gradient-to-r from-black via-[rgba(0,0,128,0.1)] to-black" id="file" value="2" max="100"></progress> <p className='text-white text-[10px]'>0.001 %</p>
         </div>
       </div>
     </div>
-    <FontAwesomeIcon color='white' className="w-screen text-[25px] text-[rgba(0,0,128)] animate-bounce justify-center absolute bottom-[100px]" icon={faChevronDown}/>
-    <FontAwesomeIcon color='white' className="w-screen text-[25px] text-[rgba(0,0,128)] animate-bounce justify-center absolute bottom-[125px]" icon={faChevronDown}/>
-    <FontAwesomeIcon color='white' className="w-screen text-[25px] text-[rgba(0,0,128)] animate-bounce justify-center absolute bottom-[150px]" icon={faChevronDown}/>
+    <FontAwesomeIcon color='white' className="w-screen text-[25px] text-white animate-bounce justify-center absolute bottom-[100px]" icon={faChevronDown}/>
+    <FontAwesomeIcon color='white' className="w-screen text-[25px] text-white animate-bounce justify-center absolute bottom-[115px]" icon={faChevronDown}/>
+    <FontAwesomeIcon color='white' className="w-screen text-[25px] text-white animate-bounce justify-center absolute bottom-[130px]" icon={faChevronDown}/>
     </div>
   );
 }
