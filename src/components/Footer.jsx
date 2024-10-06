c130dfe820d91a18d78b21576872c9bf7401eb92
abinjustinkumaravel <abinjustinkumaravel@gmail.com>
2024-10-06 14:42:39 +0530

    max changes in home page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="text-[10px] flex justify-center items-center text-white py-4 text-center">
+    <footer className="lg:text-[10px] text-[5px] flex justify-center items-center text-white py-4 text-center">
       <p>© 2024 tooClarity. All rights reserved.</p>
     </footer>
   );
